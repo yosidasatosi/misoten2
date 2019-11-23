058d865dd3120f4eda50499b1595b9d62c37302c
kurosawayuta <yu.1225.ta.mv.vm@gmail.com>
2019-11-24 00:19:45 +0900

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,41 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public new Camera camera;
+    [Range(1,4)]
+    public int PlayerNo = 1;
     public Vector3 moveRange = new Vector3(5.0f, 3.0f, 8.0f);
+    public float Speed = 10.0f;
     public float Yoffset = 0.0f;
     public float InitAnimSpeed = 1.0f;
+
+    private Camera MainCamera;
 
     // Start is called before the first frame update
     void Start()
     {
         var animator = GetComponentInChildren<Animator>();
         animator?.SetFloat("Speed", InitAnimSpeed);
+
+        MainCamera = Camera.main;
     }
 
     // Update is called once per frame
 
     private void Update()
     {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
-        transform.Translate(inputX, inputY, 0.0f);
+        Vector3 input;
+        input.x = Input.GetAxis("LeftStickX" + PlayerNo);
+        input.y = Input.GetAxis("LeftStickY" + PlayerNo);
+        input.z = 0.0f;
+
+        //float inputX = Input.GetAxis("LeftStickX"+PlayerNo);
+        //float inputY = Input.GetAxis("LeftStickY"+PlayerNo);
+        transform.Translate(input * Speed * Time.deltaTime);
     }
 
     void LateUpdate()
     {
-        Vector3 cameraPos = camera.transform.position;
+        Vector3 cameraPos = MainCamera.transform.position;
         transform.position = new Vector3
         {
             x = Mathf.Clamp(transform.position.x, cameraPos.x-moveRange.x, cameraPos.x+moveRange.x),
