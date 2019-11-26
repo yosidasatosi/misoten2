0d4eb334772ed0117adffaad45682073c272290a
Kuroa0 <mugitya.itoen@gmail.com>
2019-11-26 17:30:52 +0900

    海溝 更新 うつぼ　たこ追加

--- Assets/Scripts/MoveTako.cs
+++ Assets/Scripts/MoveTako.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class MoveTako : MonoBehaviour
 {
+    // TakoManagerスクリプト用変数
     private TakoManager takomanager;
     private GameObject gameobject;
 
-    //private GameObject MainSumiObj;
-    //private GameObject SubSumiObj;
-
-    //private ParticleSystem MainSumiPar;
-    //private ParticleSystem SubSumiPar;
-
+    // 墨のパーティクル用変数
     private ParticleSystem SumiParticle;
 
-    private float Speed;
-    private int MoveMode;
-    private float Limit;
+    // int型
+    private int MoveMode;       // 動作モード
 
-    private bool Fire;
+    // float型
+    private float Speed;        // 移動速度
+    private float StopPos;      // 停止位置
+    private float FireTime;     // 墨発射時間
+    private float FireCount;    // 墨発射経過秒数
+
+    // bool型
+    private bool Fire;          // 墨発射フラグ
+    private bool Stop;          // 停止フラグ
+    private bool ReStart;       // 再動作フラグ
 
     // Start is called before the first frame update
     void Start()
@@ -36,53 +40,95 @@ public class MoveTako : MonoBehaviour
         // 動作方法の保存
         MoveMode = takomanager.GetMoveMode();
 
-        Limit = takomanager.GetLimit();
+        // 停止位置の保存
+        StopPos = takomanager.GetStopPos();
 
+        // 墨を吐いている時間設定
+        FireTime = 5.0f;
+
+        // 墨を吐き始めてからの時間経過
+        FireCount = 0.0f;
+
+        // 墨のパーティクルコンポーネントの取得
         SumiParticle = this.GetComponent<ParticleSystem>();
+
+        // パーティクルの動作を停止
         SumiParticle.Stop();
 
+        // bool初期化
         Fire = false;
+        Stop = false;
+        ReStart = false;
+
+        // 移動方向設定
+        SetSpeed();
     }
 
     // Update is called once per frame
     void Update()
     {
-        switch(MoveMode)
+        if (!ReStart)
+        { // ReStart = false
+            CheckStop();    // 停止するかどうか
+        }
+
+        if (Stop)
+        { // Stop = true
+            if (!Fire)
+            { // Fire = false
+                //墨を吐く処理
+                SumiParticle.Play();
+
+                // 墨発射開始
+                Fire = true;
+            }
+            else
+            { // Fire = true
+                if (FireTime > FireCount)
+                { // 発射時間が発射経過時間より大きい
+                    FireCount += Time.deltaTime;
+                }
+                else
+                { // 発車時間が発射経過時間より小さい
+                    Fire = false;   // 墨発射終了
+                    Stop = false;   // 停止終了
+                    ReStart = true; // 再動作開始
+                }
+            }
+        }
+        else
+        { // Stop = false
+            // 対応した方向に移動
+            transform.Translate(0.0f, Speed * Time.deltaTime, 0.0f, Space.Self);
+        }
+    }
+
+    // 停止判定関数
+    void CheckStop()
+    {
+        switch (MoveMode)
         {
-            case 0:
-                if (transform.localPosition.y > Limit)
-                {
-                    // 下方向に移動
-                    transform.Translate(0.0f, Speed * Time.deltaTime * -1, 0.0f, Space.Self);
-                }
-                else
-                {
-                    if (!Fire)
-                    {
-                        //墨を吐く処理
-                        SumiParticle.Play();
-
-                        Fire = true;
-                    }
+            case 0: // 下方向に移動
+                if (transform.localPosition.y < StopPos)
+                { // 停止位置より小さくなった時
+                    Stop = true;    // 停止開始
                 }
                 break;
-            case 1:
-                if (transform.localPosition.y < Limit)
-                {
-                    // 上方向に移動
-                    transform.Translate(0.0f, Speed * Time.deltaTime, 0.0f, Space.Self);
-                }
-                else
-                {
-                    if (!Fire)
-                    {
-                        //墨を吐く処理
-                        SumiParticle.Play();
-
-                        Fire = true;
-                    }
+            case 1: // 上方向に移動
+                if (transform.localPosition.y > StopPos)
+                { // 停止位置より大きくなった時
+                    Stop = true;    // 停止開始
                 }
                 break;
+        }
+    }
+
+    // 移動方向設定関数
+    void SetSpeed()
+    {
+        if (MoveMode == 0)
+        {
+            Speed *= -1;
         }
     }
 }
--- Assets/Scripts/TakoManager.cs
+++ Assets/Scripts/TakoManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class TakoManager : MonoBehaviour
 {
+    // public
+    // クローンするモデル
     public List<GameObject> takoPrefab = new List<GameObject>();
 
     // 構造体を編集可能にする
@@ -12,43 +14,69 @@ public class TakoManager : MonoBehaviour
     // 構造体
     public class InitData
     {
-        public GameObject takomodel;
-        public int MoveMode;
-        public float Limit;
+        public GameObject takomodel;    // 参照するモデル
+        public int MoveMode;            // 動作方法
+        public float StopPosition;      // 停止位置
     }
 
-    public float Speed;
+    // float型変数
+    public float PopTime;   // クローン生成時間
+    public float Speed;     // 移動速度
 
+    // List型
     public List<InitData> InitDatas = new List<InitData>();
 
-    private int no = -1;
+    // private
+    // int型変数
+    private int no;         // データ設定用変数
+
+    // float型変数
+    private float count;    // 経過時間
+
+    // bool型変数
+    private bool Pop;       // 生成フラグ
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // private変数初期化
+        no = 0;
+
+        count = 0.0f;
+
+        Pop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Pop)
         {
-            for (int i = 0; i < takoPrefab.Count; i++)
+            if (count > PopTime)
             {
-                for (int j = 0; j < InitDatas.Count; j++)
+                for (int i = 0; i < takoPrefab.Count; i++)
                 {
-                    // プレファブからTakoオブジェクトを生成
-                    GameObject Tako = (GameObject)Instantiate(
-                        takoPrefab[i],            // 生成するプレファブ設定
-                        Camera.main.transform     // 親設定
-                        );
+                    for (int j = 0; j < InitDatas.Count; j++)
+                    {
+                        // プレファブからTakoオブジェクトを生成
+                        GameObject Tako = (GameObject)Instantiate(
+                            takoPrefab[i],            // 生成するプレファブ設定
+                            Camera.main.transform     // 親設定
+                            );
 
-                    // 初期位置設定
-                    Tako.transform.localPosition = InitDatas[j].takomodel.transform.localPosition;
-                    Tako.transform.localRotation = InitDatas[j].takomodel.transform.localRotation;
+                        // 初期位置設定
+                        Tako.transform.localPosition = InitDatas[j].takomodel.transform.localPosition;
+                        Tako.transform.localRotation = InitDatas[j].takomodel.transform.localRotation;
+                    }
                 }
+
+                Pop = false;
             }
+        }
+
+        if (count < PopTime)
+        {
+            count += Time.deltaTime;
         }
     }
 
@@ -61,15 +89,15 @@ public class TakoManager : MonoBehaviour
     // 動作方法取得関数
     public int GetMoveMode()
     {
-        no++;
+        //no++;
         no %= InitDatas.Count;
 
         return InitDatas[no].MoveMode;
     }
 
     // 移動限界取得関数
-    public float GetLimit()
+    public float GetStopPos()
     {
-        return InitDatas[no].Limit;
+        return InitDatas[no++].StopPosition;
     }
 }
