057a3c9b8ee91eb6fbb4478066c81b1d4f542c25
tanakataiyou <taiyo_tanaka33@yahoo.co.jp>
2019-12-01 20:08:53 +0900

    stage4のギミック追加

---
+++ Assets/Scripts/ButtonConttroller.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    [Range(1, 4)]
+    public int PlayerNo = 1;
+    public int pushCnt = 0;
+    public int pushMaxCnt = 0;
+    public bool ButtonMode;
+    private MoveTo move;
+    private bool trigger;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = GetComponent<MoveTo>();
+        ButtonMode = false;
+        trigger = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            pushCnt++;
+        }
+
+        if(ButtonMode)
+        {
+            CheckPushCnt();
+        }
+        else
+        {
+            CheckPush();
+        }
+    }
+
+    void CheckPushCnt()
+    {
+        if(pushCnt >= pushMaxCnt)
+        {
+            trigger = true;
+        }
+    }
+
+    void CheckPush()
+    {
+        if (pushCnt > 0)
+        {
+            trigger = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if(trigger)
+        {
+            move.flag = true;
+            move.moveOn = true;
+            pushCnt = 0;
+            trigger = false;
+        }
+    }
+}
---
+++ Assets/Scripts/switchCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class switchCamera : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera lastCamera;
+    public float changeTime = 0.0f;
+    private float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera.enabled = true;
+        lastCamera.enabled = false;
+        startTime = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float nowTime = Time.realtimeSinceStartup - startTime;
+
+        if ((int)nowTime == changeTime)
+        {
+            mainCamera.enabled = false;
+            lastCamera.enabled = true;
+        }
+    }
+}
