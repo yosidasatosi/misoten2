f24a3782c14a428ef77ad8145415d2c615ace3f2
kurosawayuta <yu.1225.ta.mv.vm@gmail.com>
2019-12-06 12:25:42 +0900

    titlelogo

---
+++ Assets/Scripts/TitleLogoEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleLogoEffect : MonoBehaviour
+{
+    SpriteRenderer logoSprite;
+
+    [SerializeField]
+    ParticleSystem particle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        logoSprite = GetComponent<SpriteRenderer>();
+        particle.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            logoSprite.enabled = false;
+            particle.Play();
+        }
+    }
+}
