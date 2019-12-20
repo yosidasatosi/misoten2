9f17a606bc9ddd295166939ddb51d5a279bfd0c4
tanakataiyou <taiyo_tanaka33@yahoo.co.jp>
2019-12-20 23:37:54 +0900

    ボタンアニメーションの追加

--- Assets/Scripts/ButtonAnim.cs
+++ Assets/Scripts/ButtonAnim.cs
@@ -5,19 +5,26 @@ using UnityEngine.UI;
 
 public class ButtonAnim : MonoBehaviour
 {
+    public Image image;
     private bool chFlg = false;
     private float time;
     private Texture2D texture;
+
+    void Start()
+    {
+    }
+
     void Update()
     {
         time += Time.deltaTime;
 
-        if (time > 1.0f)
+        if (time > 0.01f)
         {
             if (!chFlg)
             {
+
                 texture = Resources.Load("button_push1") as Texture2D;
-                GetComponent<Image>().sprite = Sprite.Create(texture,
+                image.sprite = Sprite.Create(texture,
                                        new Rect(0, 0, texture.width, texture.height),
                                        Vector2.zero);
                 chFlg = true;
@@ -25,7 +32,7 @@ public class ButtonAnim : MonoBehaviour
             else
             {
                 texture = Resources.Load("button_push2") as Texture2D;
-                GetComponent<Image>().sprite = Sprite.Create(texture,
+                image.sprite = Sprite.Create(texture,
                                        new Rect(0, 0, texture.width, texture.height),
                                        Vector2.zero);
                 chFlg = false;
