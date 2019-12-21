2f8ead38849f9de2b8ac9ee3c747aebf19cfe2c7
yosida <yosidasatosihal@gmail.com>
2019-12-21 19:52:30 +0900

    led test

--- Assets/Scripts/JellyfishController.cs
+++ Assets/Scripts/JellyfishController.cs
@@ -14,40 +14,45 @@ public class JellyfishController : MonoBehaviour
     private bool ParticleStop = false;
     private GameObject Particle;
     private Collider MyCollider;
+    private Renderer MyRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
         MyCollider = GetComponent<SphereCollider>();
+        MyRenderer = GetComponentInChildren<Renderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Timer += Time.deltaTime;
 
-        if (Timer >= (ParticleStop ? StopTime : TimeBeforeDisable))
+        if (MyRenderer.isVisible)
         {
-            ParticleStop = !ParticleStop;
-            if(ParticleStop)
-            {
-                Destroy(Particle);
-                LedState.Instance.Set(SceneControllerBase.Instance.DefaultLedSituation);
-            }
-            else
-            {
-                LedState.Instance.Set(LedState.Situation.FLASH);
-                Particle = Instantiate(ParticlePrefab, transform.position, Quaternion.identity);
-                Particle.transform.localScale = Vector3.one * ParticleScale;
-            }
-            if(MyCollider != null)
-            {
-                MyCollider.enabled = !ParticleStop;
+            Timer += Time.deltaTime;
 
+            if (Timer >= (ParticleStop ? StopTime : TimeBeforeDisable))
+            {
+                ParticleStop = !ParticleStop;
+                if (ParticleStop)
+                {
+                    Destroy(Particle);
+                    LedState.Instance.Set(SceneControllerBase.Instance.DefaultLedSituation);
+                }
+                else
+                {
+                    LedState.Instance.Set(LedState.Situation.FLASH);
+                    Particle = Instantiate(ParticlePrefab, transform.position, Quaternion.identity);
+                    Particle.transform.localScale = Vector3.one * ParticleScale;
+                }
+                if (MyCollider != null)
+                {
+                    MyCollider.enabled = !ParticleStop;
+
+                }
+                Timer = 0.0f;
             }
-            Timer = 0.0f;
         }
-
 
         if (transform.localPosition.x <= -10.0f)
         {
